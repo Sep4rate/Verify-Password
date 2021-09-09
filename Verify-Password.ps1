6a8417014035cc86b1a64d5495d4d003aeb0c7f5
Sep / rate <55880376+Sep4rate@users.noreply.github.com>
2021-09-09 11:57:58 +0200

    Update Verify-Password.ps1
    Version 0.8: Added comments and important output messages.

--- Verify-Password.ps1
+++ Verify-Password.ps1
@@ -1,27 +1,59 @@
+# Copyright (c) 2020
+# Licensed under the GNU General Public License v3.0
+# 
+# This script uses the "Have I Been Pwned?" API developed by Troy Hunt
+# to determine if a password is in the "Pwned Password" list.
+# 
+# In order to protect the confidentiality of the given password, 
+# the script first hashes the password using a SHA-1 algorithm and extracts
+# the first 5 characters of the hash for searching the DB. If the query
+# returns a list of hashes, it means that there's a huge possibility that
+# the password has been compromised.
+#
+# More info on the Passwords DB: https://haveibeenpwned.com/Passwords
+# 
+# Usage example:
+# .\Verify-Password.ps1 "Password123"
+#
+# ***** IMPORTANT *****
+# Pleaase do consider PowerShell history and logging while typing your
+# passwords in a PowerShell window.
+
+# Place the password in a variable and initialize the base URI
 Param(
     [string] $someString = "melobie"
 )
 $uri = 'https://api.pwnedpasswords.com/range/'
+
+# Hash the password using SHA-1
 $sha1 = New-Object -TypeName System.Security.Cryptography.SHA1CryptoServiceProvider
 $utf8 = New-Object -TypeName System.Text.UTF8Encoding
 $hash = [System.BitConverter]::ToString($sha1.ComputeHash($utf8.GetBytes($someString)))
 $hash = $hash.Replace("-","")
-Write-Output "Password SHA1 hash: $hash`n"
+$someString = $null
+Write-Host "Password SHA1 hash: $hash`n"
 
+# Split the hash in prefix and suffix
 $prefix = $hash.Substring(0,5)
 $suffix = $hash.Substring(5)
 
-Write-Output "Getting pwned passwords starting with $prefix...`n"
+# Append the prefix to the search URI and go fetch the results
+Write-Host "Getting pwned passwords starting with $prefix...`n"
 $uri = $uri + $prefix
 $searchResult = Invoke-WebRequest $uri
 
+# If the response contains the given suffix, the hash has been found!
+# The number of times it has been found appears after the : symbol.
 if ($searchResult.Content.Contains($suffix)) {
     $foundString = $searchResult.Content.Substring($searchResult.Content.IndexOf($suffix))
     $foundString = $foundString.Substring(0,$foundString.indexof("`n"))
     $howMany = $foundString.Substring($foundString.IndexOf(":"))
-    Write-Output "The given password has been pwned!"
-    Write-Output "Instances found $howMany"
+    Write-Host "The given password has been pwned!"
+    Write-Host "Instances found $howMany"
 }
 else {
-    Write-Output "Congratulations! Password not found in DB."
+    Write-Host "Congratulations! Password not found in DB."
 }
+
+# Close with a fair warning
+Write-Host "`nMake sure to run 'Clear-History' after verifying your passwords.`n" -ForegroundColor Yellow
